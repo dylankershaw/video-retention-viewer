b2cd4c18d5149d90b3c0657c4ad9fc2c54a7c21a
github username <dylankershaw@me.com>
2018-02-22 12:54:38 -0500

    revise Video#Show logic to calculate retention rather than dropoffs

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -4,11 +4,17 @@ class VideosController < ApplicationController
         
         views = View.where(["video_id = ? and date > ?", params[:id], 30.days.ago])
         
+        # determine number of dropoffs for each quarter
+        first_quarter = views.select { |view| view.completion < 0.25 }.count
+        second_quarter = views.select { |view| view.completion >= 0.25 && view.completion < 0.5 }.count
+        third_quarter = views.select { |view| view.completion >= 0.5 && view.completion < 0.75 }.count
+        fourth_quarter = views.select { |view| view.completion >= 0.75 }.count
+
         @data = {
-            "0% - 25%" => views.select { |view| view.completion < 0.25 }.count,
-            "25% - 50%" => views.select { |view| view.completion >= 0.25 && view.completion < 0.5 }.count,
-            "50% - 75%" => views.select { |view| view.completion >= 0.5 && view.completion < 0.75 }.count,
-            "75% - 100%" => views.select { |view| view.completion >= 0.75 }.count
+            "0%" => views.count,
+            "50%" => second_quarter + third_quarter + fourth_quarter,
+            "75%" => third_quarter + fourth_quarter,
+            "100%" => fourth_quarter
         }
     end
 end
