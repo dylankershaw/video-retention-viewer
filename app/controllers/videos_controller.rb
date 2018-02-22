7294177b744174b537d5437ada5afad9322a5190
github username <dylankershaw@me.com>
2018-02-22 11:38:41 -0500

    enable Video#Show to return relevant video's views from past 30 days

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,6 @@
 class VideosController < ApplicationController
     def show
         @video = Video.find(params[:id])
+        @views = View.where(["video_id = ? and date > ?", params[:id], 30.days.ago])
     end
 end
