cd8ce535c8b5d5c9ddedc393ce3caffb9a6b92a0
github username <dylankershaw@me.com>
2018-02-22 12:31:29 -0500

    enable Video#Show to split views by completion and View to display data accordingly

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,6 +1,14 @@
 class VideosController < ApplicationController
     def show
         @video = Video.find(params[:id])
-        @views = View.where(["video_id = ? and date > ?", params[:id], 30.days.ago])
+        
+        views = View.where(["video_id = ? and date > ?", params[:id], 30.days.ago])
+        
+        @data = {
+            "first quarter" => views.select { |view| view.completion < 0.25 }.count,
+            "second_quarter" => views.select { |view| view.completion >= 0.25 && view.completion < 0.5 }.count,
+            "third_quarter" => views.select { |view| view.completion >= 0.5 && view.completion < 0.75 }.count,
+            "fourth_quarter" => views.select { |view| view.completion >= 0.75 }.count
+        }
     end
 end
