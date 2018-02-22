ec55e37eca1bf422f01705045b7a109cd80272b1
github username <dylankershaw@me.com>
2018-02-22 10:02:34 -0500

    generate seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+100.times do
+    User.create
+end
+
+10.times do
+    Video.create
+end
+
+def createViews(num, min, max)
+    num.times do
+        View.create(
+            user_id: rand(User.first.id..User.last.id),
+            video_id: rand(Video.first.id..Video.last.id),
+            date: Time.at(rand(Time.new(2018).to_i..Time.now.to_i)),
+            completion: (rand(min..max) / 100).round(2)
+        )
+    end
+end
+
+# 0% - 25% completions
+createViews(400, 0, 24.9)
+
+# 25% - 50% completions
+createViews(300, 25.0, 49.9)
+
+# 50% - 75% completions
+createViews(200, 50.0, 74.9)
+
+# 75% - 100% completions
+createViews(100, 75.0, 100.0)
