4c7b2710a6b1c6650411c2dba31c1b060e470a48
github username <dylankershaw@me.com>
2018-02-22 12:44:39 -0500

    rename Video#Show data keys and add axis titles to view chart

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -5,10 +5,10 @@ class VideosController < ApplicationController
         views = View.where(["video_id = ? and date > ?", params[:id], 30.days.ago])
         
         @data = {
-            "first quarter" => views.select { |view| view.completion < 0.25 }.count,
-            "second_quarter" => views.select { |view| view.completion >= 0.25 && view.completion < 0.5 }.count,
-            "third_quarter" => views.select { |view| view.completion >= 0.5 && view.completion < 0.75 }.count,
-            "fourth_quarter" => views.select { |view| view.completion >= 0.75 }.count
+            "0% - 25%" => views.select { |view| view.completion < 0.25 }.count,
+            "25% - 50%" => views.select { |view| view.completion >= 0.25 && view.completion < 0.5 }.count,
+            "50% - 75%" => views.select { |view| view.completion >= 0.5 && view.completion < 0.75 }.count,
+            "75% - 100%" => views.select { |view| view.completion >= 0.75 }.count
         }
     end
 end
