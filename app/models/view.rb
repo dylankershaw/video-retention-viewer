c44f50be2dd8d94855838de32fd0fe73b04f6d7f
github username <dylankershaw@me.com>
2018-02-23 14:16:01 -0500

    implement View model validations

--- app/models/view.rb
+++ app/models/view.rb
@@ -1,4 +1,6 @@
 class View < ApplicationRecord
     belongs_to :user
     belongs_to :video
+    validates :user, :video, :date, :completion, presence: true
+    validates :completion, :inclusion => {:in => [0,1.0]}
 end
