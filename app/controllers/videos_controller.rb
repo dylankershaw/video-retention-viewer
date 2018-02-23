fee1c6c33210c0f16424db4523dce7f278b8aeb5
github username <dylankershaw@me.com>
2018-02-23 10:45:18 -0500

    refactor View data sorting logic to use select() directly rather than adding dropoff rates

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,17 +3,13 @@ class VideosController < ApplicationController
         @video = Video.find(params[:id])
         
         @views = View.where(["video_id = ? and date > ?", params[:id], 30.days.ago])
-        
-        # determine number of dropoffs for each quarter
-        second_quarter = @views.select { |view| view.completion >= 0.25 && view.completion < 0.5 }.count
-        third_quarter = @views.select { |view| view.completion >= 0.5 && view.completion < 0.75 }.count
-        fourth_quarter = @views.select { |view| view.completion >= 0.75 }.count
 
         @data = {
             "0%" => @views.count,
-            "50%" => second_quarter + third_quarter + fourth_quarter,
-            "75%" => third_quarter + fourth_quarter,
-            "100%" => fourth_quarter
+            "25%" =>  @views.select { |view| view.completion >= 0.25 }.count,
+            "50%" => @views.select { |view| view.completion >= 0.5 }.count,
+            "75%" => @views.select { |view| view.completion >= 0.75 }.count,
+            "100%" => @views.select { |view| view.completion === 1 }.count
         }
     end
 end
