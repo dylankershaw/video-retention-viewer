5747925626ff01142ac115c208ed66d7c19b0170
github username <dylankershaw@me.com>
2018-02-24 12:30:32 -0500

    implement View model tests

--- spec/models/view_spec.rb
+++ spec/models/view_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe View, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    described_class.create(
+      user: User.create,
+      video: Video.create,
+      date: Time.now,
+      completion: 0.5
+    )
+  }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a user" do
+    subject.user = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a video" do
+    subject.video = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a date" do
+    subject.date = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a completion" do
+    subject.completion = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with a completion greater than 1" do
+    subject.completion = 1.5
+    expect(subject).to_not be_valid
+  end
 end
